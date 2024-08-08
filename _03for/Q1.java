b6d2c089eac6ad5dae2471503b6ed24f76b37808
CLee <1243qw6@gmail.com>
2024-08-08 17:34:57 +0900

    0808

--- _03for/Q1.java
+++ _03for/Q1.java
@@ -6,21 +6,21 @@ public class Q1 {
 		// TODO Auto-generated method stub
 		int[] a= {30, 40, 50, 55, 65};
 		
-		/* for(int i = 4; i >= 0 ; i--) {
+		for(int i = 4; i >= 0 ; i--) {
 			System.out.println(a[i]);
-		} */
-		/* for (int i = 0; i <= 4; i++) {
+		} 
+		for (int i = 0; i <= 4; i++) {
 		    System.out.println(a[(i+1)%5]);		// %를 중요하게 생각할 것
-		} */
-		/* for(int i = 0; i  < 5; i++) {			// 배열 순회를 반복하면서 값의 조건판단 후 출력
+		} 
+		for(int i = 0; i  < 5; i++) {			// 배열 순회를 반복하면서 값의 조건판단 후 출력
 			if(a[i] >= 50) {
 				System.out.println(a[i]);
 			}
-		} */
+		}
 		
-		int sum = 0;
-		int count = 0;
-		int minValue = 10000;
+		//int sum = 0;
+		//int count = 0;
+		//int minValue = 10000;
 		/* for(int i = 0; i < 5; i++) {
 			if(a[i] >= 50) {
 				sum += a[i];
@@ -29,7 +29,7 @@ public class Q1 {
 		}*/
 		//System.out.println("문제 1: " + sum);
 		//System.out.println("문제 2: " + (double)sum/count);
-		for(int i = 0; i < 5; i++) {
+		/* for(int i = 0; i < 5; i++) {
 			if(minValue > a[i]) {
 				minValue = a[i];
 			}
@@ -40,6 +40,6 @@ public class Q1 {
 		}
 
 		System.out.println(minValue);
-		System.out.println((double)sum/count);
+		System.out.println((double)sum/count); */
 	}
 }
---
+++ _03for/test01.java
@@ -0,0 +1,97 @@
+package _03for;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("문제 1");
+		int oddSum = 0;						// 홀수의 합 초기 값
+		int evenSum = 0;					// 짝수의 합 초기 값
+		int oddCnt = 0;						// 홀수의 갯수 초기 값
+		for(int i = 1; i <=100; i++) {
+			if(i % 2 == 0) {				
+				evenSum += i;				// 짝수의 합
+			} else {
+				oddSum += i;				// 홀수의 합
+				oddCnt ++;					// 홀수의 갯수
+			}
+		}
+		System.out.println(oddSum - evenSum);	// 홀수의 합 - 짝수의 합
+		
+	System.out.println("문제 2");
+		System.out.println(oddSum);				// 홀수의 합
+		System.out.println(evenSum);			// 짝수의 합
+		
+	System.out.println("문제 3");
+		System.out.println(oddCnt);				// 홀수의 갯수
+		
+	System.out.println("문제 4");	
+		int allNumber = 0;						// 100 ~ 999 합의 초기 값
+		for(int i = 100; i <= 999; i++) {
+			allNumber += i;						// 100 ~ 999 까지의 합
+			}
+		System.out.println((allNumber/100000) + (allNumber/10000) % 10 + 
+				(allNumber/1000)%10	+ (allNumber/100)%10 + (allNumber/10)%10);
+		// 100 ~ 999 까지의 숫자의 합의 100의 자리, 10의 자리 1의 자리의 합
+		
+	System.out.println("문제 5");
+		for(int i = 100; i <=999; i++) {
+			if((i/10) % 10 % 3 == 0) {				// 100에서 999 중 십의 자리가 6인 수
+				System.out.print(i + " ");			// 의 반복적인 나열
+			}
+		}
+	System.out.println("문제 6");
+		int accumulated = 0;					// 0 부터 100까지의 합 초기 값
+		int countN = 0;							// 0 부터 100까지 더하는 횟수 값
+		for(int i = 0; i <= 100; i++) {
+			accumulated += i;
+			if(accumulated < 44) {
+			countN ++;
+			}
+		}
+		System.out.println(countN);				// 9번째 횟수 때 44가 넘어감
+	
+	System.out.println("문제 7");
+	int sumAll = 0;
+	for(int i = 0; i <=10; i++) {
+		sumAll += i;
+		System.out.print(i + "+");				// 줄이 바뀌지 않는 숫자들
+	}
+	System.out.println(sumAll);					// 결과 값
+	
+	System.out.println("문제 8");
+		int fib = 1;
+		int aFib = 0;
+		int pFib = 0;
+		int fibSum = 0;
+		for(int i = 1; i < 8; i++) {
+			System.out.print(fib);
+			System.out.print("+");
+			fibSum += fib;
+			aFib = fib + pFib;
+			pFib = fib;
+			fib = aFib;
+		}
+		System.out.println("=" + fibSum);
+	
+	System.out.println("문제 9");
+		int fibA = 0;
+		int fibP = 0;
+		for(int i = 1; i < 20; ) {
+			System.out.print(i + " ");
+			fibA = i + fibP;
+			fibP = i;
+			i = fibA;
+		}
+	System.out.println("");
+	System.out.println("문제 10");
+	int sumofNumber = 0;
+	for(int i = 100000000; i >=1; i /= 10) {
+		System.out.print((987654321/i) % 10);
+		System.out.print("+");
+		sumofNumber += (987654321/i % 10);
+	}
+	System.out.println("=" + sumofNumber);
+	}
+}
