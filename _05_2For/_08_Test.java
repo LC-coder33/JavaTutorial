d3f59c6931db95d85e9804cb282bc31496509d47
CLee <1243qw6@gmail.com>
2024-08-19 12:46:58 +0900

    0819

--- _05_2For/_08_Test.java
+++ _05_2For/_08_Test.java
@@ -4,8 +4,15 @@ public class _08_Test {
 
 	public static void main(String[] args) {
 		int[] pang = {1,0,0,0,2,3,4,4,6,2,2,2,2,5};
+		int cnt = 0;
 		for(int i = 0; i < pang.length; i++) {
-			
+			for(int j = 1; j <pang.length; j++) {
+				if(pang[i] == pang[j] ) {
+					cnt++;
+				} else {
+					cnt = 0;
+				}
+			}
 		}
 	}
 
