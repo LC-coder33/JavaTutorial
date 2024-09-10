24c524f66a186eb6a79dfb2f27079852185324c1
leetr <onevision93@naver.com>
2024-09-10 13:56:53 +0900

    0920-2

--- test03/jobTime.java
+++ test03/jobTime.java
@@ -18,5 +18,16 @@ public class jobTime {
 			}
 		}
 		System.out.println("num 배열의 총합: " + total);
+		
+		
+		int [][] a= {{1,2,3},{4,5,6}};
+		for(int i = 0; i < a.length; i++) {
+			System.out.println();
+			for(int j=0; j < a[i].length; j++) {
+				
+				System.out.print("" + a[i][j] + " ");
+			}
+		}
 	}
-};
+}
+
